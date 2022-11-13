4362dc9fc2e26ac7072936393d8b4bcc169fc4dc
zephyr Lin <zephyrlin33@gmail.com>
2022-11-13 13:23:14 +0800

    add unique flag in models/shorten

--- models/shorten.js
+++ models/shorten.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const shortenSchema = new Schema({
-  input: { type: String, require: true},
-  output: { type: String},
+  input: { type: String, require: true,unique: true},
+  output: { type: String, require: true,unique: true},
   
 })
 
